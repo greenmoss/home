be842ec23c4d4c559ff0e0229f1dc85831470f19
Kurt Yoder <greenmoss@users.noreply.github.com>
2020-01-14 12:27:39 -0500

    Update/install vim plugins

--- .vim/install_all.sh
+++ .vim/install_all.sh
@@ -6,6 +6,19 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 # pathogen
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-# detectindent
+# install or update all vim plugins
 cd ~/.vim/bundle
-git clone https://github.com/ciaranm/detectindent
+for repo in \
+  "https://github.com/ciaranm/detectindent" \
+  "https://github.com/plasticboy/vim-markdown" \
+
+do
+  mydir=$(basename $repo)
+  if [ -d "$mydir" ]; then
+    cd "$mydir"
+    git pull origin master
+    cd -
+  else
+    git clone "$repo"
+  fi
+done
