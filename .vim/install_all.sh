69192161b77026d614cb372601d8a54b70550ac0
Kurt Yoder <ktygithub@yoderhome.com>
2022-10-04 20:16:29 -0400

    Add vim plugin vim-ps1

--- .vim/install_all.sh
+++ .vim/install_all.sh
@@ -10,8 +10,9 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 for repo in \
   "https://github.com/ciaranm/detectindent" \
-  "https://github.com/preservim/nerdtree" \
   "https://github.com/plasticboy/vim-markdown" \
+  "https://github.com/PProvost/vim-ps1" \
+  "https://github.com/preservim/nerdtree" \
 
 do
   mydir=$(basename $repo)
